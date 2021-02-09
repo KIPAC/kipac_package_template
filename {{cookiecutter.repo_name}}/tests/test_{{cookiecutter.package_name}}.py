187a13c736ceb17e9f85dde604d667d2df2cab12
Eric Charles <echarles@gmail.com>
2021-02-09 10:46:08 -0800

    Update test_{{cookiecutter.package_name}}.py

--- {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.package_name}}.py
+++ {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.package_name}}.py
@@ -2,9 +2,9 @@
 Example unit tests for {{cookiecutter.repo_name}} package
 """
 import unittest
-import desc.{{cookiecutter.repo_name|lower}}
+import {{cookiecutter.package_name}}
 
-class {{cookiecutter.repo_name}}TestCase(unittest.TestCase):
+class ExampleTestCase(unittest.TestCase):
     def setUp(self):
         self.message = 'Hello, world'
 
@@ -12,12 +12,12 @@ class {{cookiecutter.repo_name}}TestCase(unittest.TestCase):
         pass
 
     def test_run(self):
-        foo = desc.{{cookiecutter.repo_name|lower}}.{{cookiecutter.repo_name}}(self.message)
+        foo = {{cookiecutter.package_name}}.Example(self.message)
         self.assertEqual(foo.run(), self.message)
 
     def test_failure(self):
-        self.assertRaises(TypeError, desc.{{cookiecutter.repo_name|lower}}.{{cookiecutter.repo_name}})
-        foo = desc.{{cookiecutter.repo_name|lower}}.{{cookiecutter.repo_name}}(self.message)
+        self.assertRaises(TypeError, {{cookiecutter.package_name}}.{{cookiecutter.repo_name}})
+        foo = {{cookiecutter.package_name}}.Example(self.message)
         self.assertRaises(RuntimeError, foo.run, True)
 
 if __name__ == '__main__':
